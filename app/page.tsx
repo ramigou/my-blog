14a0e0bc8d65d3ac70bd78fcd4d576adba135660
ramigou <dasul1021@gmail.com>
2023-09-05 06:40:04 +0900

    feat: get sanity contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 import Image from 'next/image'
+import { createClient } from '@sanity/client'
+import { projectId, dataset, apiVersion } from '@/sanity/env'
 
-export default function Home() {
-  return <h1>hello my blog</h1>
+const client = createClient({
+  projectId,
+  dataset,
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion, // use current date (YYYY-MM-DD) to target the latest API version
+})
+
+export default async function Home() {
+  const data = await client.fetch<any[]>(
+    `*[_type == "post"]`,
+    {}
+    // {
+    //   // You can set any of the `cache` and `next` options as you would on a standard `fetch` call
+    //   cache: 'force-cache',
+    //   next: { tags: ['pages'] },
+    // }
+  )
+
+  const list = data.map((d) => <li key={d._id}>{d.title}</li>)
+
+  return <ul>{list}</ul>
 }
